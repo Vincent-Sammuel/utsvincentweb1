6443c01ad33656a5ecb3f50cf7d7580628a8905e
Vincent-Sammuel <vincentsammuel00@gmail.com>
2024-11-22 15:09:31 +0700

    first commit

--- script.js
+++ script.js
@@ -1,47 +1,19 @@
-// Event Listener untuk Form Login
-if (document.getElementById('loginForm')) {
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-  
-      const usernameInput = document.getElementById('username').value;
-      const passwordInput = document.getElementById('password').value;
-  
-      const storedUsername = localStorage.getItem('username');
-      const storedPassword = localStorage.getItem('password');
-  
-      if (usernameInput === storedUsername && passwordInput === storedPassword) {
-        // Jika login berhasil
-        window.location.href = 'menu.html';
-      } else {
-        // Jika login gagal
-        document.getElementById('error-message').style.display = 'block';
-      }
-    });
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+    
+  const hardcodedUsername = 'admin123';
+  const hardcodedPassword = '12345';
+
+  const usernameInput = document.getElementById('username').value;
+  const passwordInput = document.getElementById('password').value;
+
+  if (usernameInput === hardcodedUsername && passwordInput === hardcodedPassword) {
+    
+    alert('Login berhasil! Anda akan diarahkan ke halaman menu.');
+    window.location.href = 'menu.html';
+  } else {
+      
+    document.getElementById('error-message').style.display = 'block';
   }
-  
-  // Event Listener untuk Form Register
-  if (document.getElementById('registerForm')) {
-    document.getElementById('registerForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-      
-      // Ambil nilai input dari form
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
-      const confirmPassword = document.getElementById('confirm-password').value;
-      
-      // Validasi kata sandi
-      if (password !== confirmPassword) {
-        alert('Kata sandi dan konfirmasi kata sandi harus sama.');
-        return;
-      }
-      
-      // Simpan data ke Local Storage
-      localStorage.setItem('username', username);
-      localStorage.setItem('password', password);
-      
-      // Alihkan pengguna ke halaman login
-      alert('Registrasi berhasil! Silakan login.');
-      window.location.href = 'login.html';
-    });
-  }
-  
+});
